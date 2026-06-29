f1286039c14982fe4dfbf9d773812c2ef5af4579
huuthang270706-bit <huuthang270706@gmail.com>
2026-06-29 20:49:48 +0700

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+package vn.edu.student;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven!");
+        System.out.println("Xin chào Java hiện đại!");
+    }
+}
---
+++ src/com/example/lab1/Lab1Application.java
@@ -0,0 +1,10 @@
+package com.example.lab1;
+
+@SpringBootApplication
+public class Lab1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab1Application.class, args);
+    }
+
+}
